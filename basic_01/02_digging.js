06922c7f0307f644b1c2bf67b32171d396414fb8
anmolsingh1012 <77225755+anmolsingh1012@users.noreply.github.com>
2024-07-09 20:01:20 +0000

    get to know about fns

---
+++ basic_01/02_digging.js
@@ -0,0 +1,27 @@
+
+let number = 33
+//console.log(typeof number);
+//console.log(typeof(number));
+//console.log(isNumber);
+let isNumber = String(number)
+
+// datatypes summary
+
+
+const bigNumber= 1761768188814n
+console.log(typeof bigNumber);
+
+const heros = ["shaktiman", "captain america","logggg"]
+let obj1 = {
+      name :"anmol",
+      age :22,
+}
+
+let nfunction = function(){
+    console.log("heheheheheh");
+}
+
+console.log(typeof heros);
+console.log(typeof obj1);
+console.log(typeof nfunction);
+
