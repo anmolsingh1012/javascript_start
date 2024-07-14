82f0a2e63fccbf3b920c7e352f50f78cba804b25
anmolsingh1012 <77225755+anmolsingh1012@users.noreply.github.com>
2024-07-14 20:54:27 +0000

    arrays

---
+++ basic_02/01_arrays.js
@@ -0,0 +1,17 @@
+const arr1 = [2,3,344,74,234]
+console.log(typeof arr1);
+//console.log(arr1.toLocaleString());
+ const arr2 = new Array(12,13,14,11,9)
+//arr1.unshift(4) // add 4 to the starting index of the array
+//console.log(arr1);
+
+//slice and splice
+
+console.log("A" , arr1);
+console.log("slice walaa ",arr1.slice(0,2))
+console.log("B ",arr1);
+console.log(" Splice walaa",arr1.splice(0,2));
+console.log("c ",arr1);
+
+
+
