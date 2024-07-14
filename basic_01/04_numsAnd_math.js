6ef907c82250ca375e67943f42d39e0708f0ee20
anmolsingh1012 <77225755+anmolsingh1012@users.noreply.github.com>
2024-07-14 10:03:31 +0000

    Dice value geneator with the help of Math method

---
+++ basic_01/04_numsAnd_math.js
@@ -0,0 +1,26 @@
+const score = new Number(129.556)
+//console.log(score);
+//console.log(`length is: ${score.toString.length}`);
+//console.log(score.toFixed(2));
+//
+//console.log(score.toPrecision(3));
+//const scoree = 1000000
+//console.log(scoree.toLocaleString('en-IN'));
+
+//**************Math 
+
+console.log(Math);
+console.log(typeof Math);
+console.log(Math.abs(-100));
+console.log(Math.round(5.670))
+console.log(Math.floor(4.9))
+console.log(Math.ceil(7.1));
+console.log(Math.random())   // 0.02222
+
+
+// dice value generator
+const min =1;
+const max =6;
+console.log(Math.floor(Math.random()*(max - min + 1)) + min );
+
+
