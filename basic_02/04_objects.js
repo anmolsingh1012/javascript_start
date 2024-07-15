96ab2fc57d0f9d7c4d967bf583ef0aecd513d1ed
anmolsingh1012 <77225755+anmolsingh1012@users.noreply.github.com>
2024-07-15 13:44:42 +0000

    object destructuring

---
+++ basic_02/04_objects.js
@@ -0,0 +1,10 @@
+// object destructuring
+
+const course= {
+    coursename : "js in hindi",
+    courseInstrucor : "annnni",
+    courseFees : "fees"
+}
+const {courseInstrucor : yo} = course
+console.log(yo);
+
