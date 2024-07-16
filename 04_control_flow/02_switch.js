5fa3964da9c79b2437b714ff98ff3f0967fe8e51
anmolsingh1012 <77225755+anmolsingh1012@users.noreply.github.com>
2024-07-16 21:05:24 +0000

    switch case

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,18 @@
+switch("march"){
+    case "jan":
+        console.log("Month is january");
+        break;
+    case "feb":
+        console.log("Month is february");
+        break;
+    case "march":
+        console.log("Month is march");
+        break;
+    case "april":
+        console.log("Month is april");
+        break;
+    default :
+        console.log("koi b ni mila, nothing found");
+        break;
+
+}
