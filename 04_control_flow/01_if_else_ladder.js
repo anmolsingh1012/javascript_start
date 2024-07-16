904ebc4139ef44c835b5b8649e7559b7048d93b1
anmolsingh1012 <77225755+anmolsingh1012@users.noreply.github.com>
2024-07-16 20:46:52 +0000

    basic if else statement

---
+++ 04_control_flow/01_if_else_ladder.js
@@ -0,0 +1,20 @@
+const score =1000
+
+if (score <500) {
+    console.log("Less than 500");
+}
+else if(score <1200){
+    console.log("less than 1200");  
+// only this will execute even other condition are true but our code finds this instruction first, that's why it will execute only this statement
+}
+
+else if(score >500){
+    console.log("haan bhai");
+}
+
+else{
+    console.log("krr khtm");
+}
+
+
+    
