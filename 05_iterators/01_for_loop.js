cbdc49294004b9d81225a06c59c0143b5ac182ff
anmolsingh1012 <77225755+anmolsingh1012@users.noreply.github.com>
2024-07-17 14:57:42 +0000

    for loop

--- 05_iterators/01_for_loop.js
+++ 05_iterators/01_for_loop.js
@@ -1,1 +1,11 @@
-console.log("hahahah");
+for(let i =1 ; i<=10; i++){
+    console.log(`Multiplication of ${i}`);
+    
+    for(let j = 1 ; j<=10; j++){
+       console.log(i + '*' + j + '=' + i*j );
+    }
+}
+
+ 
+
+
