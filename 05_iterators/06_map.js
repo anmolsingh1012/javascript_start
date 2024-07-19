1382896d2324b1cd6ffd7bab0cec07c8c540e9d5
anmolsingh1012 <77225755+anmolsingh1012@users.noreply.github.com>
2024-07-19 07:01:26 +0000

    map method

---
+++ 05_iterators/06_map.js
@@ -0,0 +1,13 @@
+const myNumbers = [ 1,2,3,4,5,6,7,8,9,10]
+//const luck=myNumbers.map( (yo) => yo +10 )
+
+//chaining
+const luck = myNumbers
+        .map( (num) => num*10 )
+        .map( (num) => num - 10)
+        .filter( (num) => num >70)
+
+console.log(luck);
+
+
+
